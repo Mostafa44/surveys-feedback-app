20acfe142e3a1cebcdb8f4bec95f8e332506610a
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-02-04 22:14:12 +0200

    add the step to save the google id profile and resolve the error of order of passport and models in index.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy= require('passport-google-oauth20').Strategy;
+const mongoose= require('mongoose');
+const User= mongoose.model('users');
 const keys= require('../config/keys');
 
 passport.use(
@@ -7,9 +9,10 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL:'/auth/google/callback'
-    },(accessToken, refreshToken, done)=>{
+    },(accessToken, refreshToken, profile,done)=>{
         console.log(accessToken);
         console.log(refreshToken);
         console.log(done);
+        new User({googleId:profile.id}).save();
     })
     );
