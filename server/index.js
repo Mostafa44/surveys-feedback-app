d5f1ac4cf161e70d5cb8ec139c08c3fc3fa2adfd
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-02-04 21:19:18 +0200

    add mongoose and use it to connect to the mondodb instance created on the cloud

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express= require('express');
+const mongoose= require('mongoose');
+const keys= require('./config/keys');
 require('./services/passport');
 const authRoutes= require('./routes/authRoutes');
 const app= express();
 
+mongoose.connect(keys.mongoURI);
 authRoutes(app);
 
 const PORT= process.env.PORT || 5000;
