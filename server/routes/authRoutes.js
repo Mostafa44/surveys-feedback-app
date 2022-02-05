e8579ce123ecdb8f1ff3b1e0c8b0e630d8366c15
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-02-05 12:27:44 +0200

    add a route to validate the authentication after passing thru the middleware steps

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,4 +5,10 @@ app.get('/auth/google',passport.authenticate('google',{
 
 }));
 app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get('/api/current_user', (req, res)=>{
+    res.send(req.user);
+})
+
 }
+
