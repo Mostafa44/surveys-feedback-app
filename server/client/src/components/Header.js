7afde3b83f8cef599b45668132cf60b3b44523e1
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-02-18 18:27:41 +0200

    initial styling of the header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,9 +2,14 @@ import React,{useState} from 'react';
 
 const Header=()=>{
     return (
-        <div>
-            Header
-        </div>
+       <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Emaily</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a>Login with Google</a></li>
+      </ul>
+    </div>
+  </nav>
     )
 }
 
