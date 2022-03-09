fbee534a46430c26d4f318d9fe7a777ba7e7dac3
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-03-09 16:15:41 +0200

    add send-grid logic to backend

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -2,7 +2,10 @@ const requireLogin= require('../middlewares/requireLogin');
 const requireCredits= require('../middlewares/requireCredits');
 const mongoose= require('mongoose');
 const recipientSchema = require('../models/Recipient');
+const Mailer = require('../services/Mailer');
+const surveyTemplate= require('../services/emailTemplates/surveyTemplate');
 const Survey=  mongoose.model('surveys');
+
 module.exports=(app)=>{
 
     app.post('/api/surveys', requireLogin,requireCredits,(req, res)=>{
@@ -11,10 +14,13 @@ module.exports=(app)=>{
                 title,
                 subject,
                 body,
-                recipients= recipients.split(',').map(email=> ({email:email.trim()})),
+                recipients: recipients.split(',').map(email=> ({email:email.trim()})),
                 dateSent: Date.now(),
                 _user: req.user.id,
             });
+            const mailer= new Mailer(survey, surveyTemplate(survey) );
+            mailer.send();
+
     })
 
 }
